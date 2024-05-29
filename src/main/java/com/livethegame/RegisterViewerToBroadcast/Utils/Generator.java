49a20997a17483939c3623e8f6a62ab358c81560
Julian Rodriguez <jurodr@bancolombia.com.co>
2024-05-29 16:30:09 -0500

    fix generador de codigos

--- src/main/java/com/livethegame/RegisterViewerToBroadcast/Utils/Generator.java
+++ src/main/java/com/livethegame/RegisterViewerToBroadcast/Utils/Generator.java
@@ -3,6 +3,6 @@ package com.livethegame.RegisterViewerToBroadcast.Utils;
 public  class Generator {
     public static String generateCode(){
         double code = 100000 + Math.random() * 900000;
-        return String.valueOf(code);
+        return String.valueOf(code).substring(0,6);
     };
 }
